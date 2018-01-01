1211023a325f3c95685d8fdc11322fa8c9210879
Nicky Chung <nickychung@gmail.com>
2018-01-01 16:46:46 +0800

    not yet done, file open, read and print on screen

--- my-first-python.py
+++ my-first-python.py
@@ -7,7 +7,22 @@ def main():
     print (os.getcwd())
     os.get_terminal_size()
     screenWidth = os.get_terminal_size().columns
-    print ("a".center(screenWidth))
+    openedFile = FileOpen()
+    if(openedFile!=0):
+        for line in openedFile:
+            print(line)
+    openedFile.close()
     return 0
+
+def FileOpen():
+    f = open('./test.txt',mode='w')
+    if(f.readable):
+        print("Read OK")
+        return f
+    else:
+        print("Read Fail")
+        return 0
+
+
 
 main()
