87a484b4f0503982022e0473470d64688bb3f5ce
Nicky Chung <nickychung@gmail.com>
2018-07-22 17:37:13 +0800

    added formFiller.py

--- my-first-python.py
+++ my-first-python.py
@@ -4,19 +4,20 @@ import os
 def main():
    # print "hello world"
     a = 3
+    fileName = 'test.txt'
     print (os.getcwd())
     os.get_terminal_size()
     screenWidth = os.get_terminal_size().columns
-    openedFile = FileOpen()
+    openedFile = FileOpen(fileName)
     if(openedFile!=0):
         for line in openedFile:
             print(line)
         openedFile.close()
     return 0
 
-def FileOpen():
+def FileOpen(fileToOpen):
     try:
-        f = open('./test.txt',mode='r+')
+        f = open(fileToOpen,mode='r')
         print("Read OK")
         return f
     except FileNotFoundError:
